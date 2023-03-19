c6187774c7459432c6874fd8c56619217a1c5843
teguh <teguharia172@gmail.com>
2023-03-19 22:13:09 +0700

    add function lesson & exercise

---
+++ src/main/scala/part1/basicpackage/Functions.scala
@@ -0,0 +1,33 @@
+package part1.basicpackage
+
+object Functions extends App{
+
+  def aFunction (a: String, b: Int) : String = {
+    a + " " + b
+  } // return type do not need to be specify for non recursive function
+  // for best practice always specify the return type of a function
+
+  println(aFunction("hello", 3))
+
+  def aParameterlessFuction(): Int = 42
+  println(aParameterlessFuction())
+  //  println(aParameterlessFuction) this method will work with scala 2 however with scala 3
+  //  you will still need the parentheses of when calling a parameterless function
+
+  def aRepeatedFunction(aString : String, n: Int): String = {
+    if (n == 1) aString
+    else aString + aRepeatedFunction(aString, n-1)
+  }
+
+  println(aRepeatedFunction("hello",3))
+
+  // WHEN YOU NEED LOOPS, USE RECURSION.
+
+  def aFunctionWithSideEffects(aString: String): Unit = println(aString)
+
+  def aBigFunction(n: Int): Int = {
+    def aSmallerFunction(a : Int, b: Int) : Int = a + b
+
+    aSmallerFunction(n,n-1)
+  }
+}
---
+++ src/main/scala/part1/exercise/functionExercise.scala
@@ -0,0 +1,15 @@
+package part1.exercise
+
+object functionExercise extends App{
+
+  /*
+  Question :
+    1. Write a greeting function for kids (name, age) "Hi, my name is $name and I am $age years old
+    2. Factorial function 1 * 2 * 3 * ... * n (Hint: recursive function)
+    3. A Fibonacci function
+      f(1) = 1
+      f(2) = 1
+      f(n) = f(n-1) + f(n-2)
+    4. Test if a number is prime
+  */
+}
